fe2a1b8188008b94cb76c3d585dd6669262fb538
analebyodkina <dp030992cad@gmail.com>
2023-12-21 16:38:58 +0200

    added o-scroll when spinner is working

--- src/js/modules/spinner.js
+++ src/js/modules/spinner.js
@@ -2,11 +2,14 @@
 function checkSpinner() {
     const video = document.querySelector('.factor-section__video');
     const spinner = document.getElementById("spinner");
+    const body = document.getElementById("body");
 
     video.onclick = function () {
         spinner.classList.add("_active");
+        body.classList.add("no-scroll");
         setTimeout (() => {
             document.getElementById("spinner").classList.remove("_active"); 
+            document.getElementById("body").classList.remove("no-scroll");
           }, 3000);
     }
     
