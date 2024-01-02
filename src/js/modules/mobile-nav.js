a9d153eb9dfa0d8651778a31eca6a3a808f60103
analebyodkina <dp030992cad@gmail.com>
2024-01-02 12:07:43 +0200

    adaptive apdates

--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -1,26 +1,25 @@
-const nav = document.querySelector('.mobile-nav');
+const nav = document.querySelector(".mobile-nav");
 
 function mobileNav() {
-	// Mobile nav button
-	const navBtn = document.querySelector('.mobile-nav-btn');	
-	const menuIcon = document.querySelector('.nav-icon');
-	const links = document.querySelectorAll('.moving');
+  // Mobile nav button
+  const navBtn = document.querySelector(".mobile-nav-btn");
+  const menuIcon = document.querySelector(".nav-icon");
+  const links = document.querySelectorAll(".moving");
 
-	navBtn.onclick = function () {		
-		nav.classList.toggle('mobile-nav--open');
-		menuIcon.classList.toggle('nav-icon--active');
-		document.getElementById("body").classList.toggle("no-scroll");
-	};
+  navBtn.onclick = function () {
+    nav.classList.toggle("mobile-nav--open");
+    menuIcon.classList.toggle("nav-icon--active");
+    document.getElementById("body").classList.toggle("no-scroll");
+  };
 
-	// Add click event listeners to each link
-	links.forEach(function (link) {
-		link.onclick = function () {
-		  nav.classList.remove('mobile-nav--open');
-		  document.body.classList.remove('no-scroll');
-		  menuIcon.classList.remove('nav-icon--active');
-		};
-		
-	  });
+  // Add click event listeners to each link
+  links.forEach(function (link) {
+    link.onclick = function () {
+      nav.classList.remove("mobile-nav--open");
+      document.body.classList.remove("no-scroll");
+      menuIcon.classList.remove("nav-icon--active");
+    };
+  });
 }
 
 export default mobileNav;
--- src/js/modules/spinner.js
+++ src/js/modules/spinner.js
@@ -1,18 +1,16 @@
-
 function checkSpinner() {
-    const video = document.querySelector('.factor-section__video');
-    const spinner = document.getElementById("spinner");
-    const body = document.getElementById("body");
+  const video = document.querySelector(".factor-section__video");
+  const spinner = document.getElementById("spinner");
+  const body = document.getElementById("body");
 
-    video.onclick = function () {
-        spinner.classList.add("_active");
-        body.classList.add("no-scroll");
-        setTimeout (() => {
-            document.getElementById("spinner").classList.remove("_active"); 
-            document.getElementById("body").classList.remove("no-scroll");
-          }, 3000);
-    }
-    
-  }
+  video.onclick = function () {
+    spinner.classList.add("_active");
+    body.classList.add("no-scroll");
+    setTimeout(() => {
+      document.getElementById("spinner").classList.remove("_active");
+      document.getElementById("body").classList.remove("no-scroll");
+    }, 3000);
+  };
+}
 
-  export default checkSpinner;
+export default checkSpinner;
